305979767ae884244995dc06de10ca4b8c867ee3
unknown <rgutie17@ucsc.edu>
2023-06-11 23:00:46 -0500

    Transform gradient

--- gpr_log_lik_gra.R
+++ gpr_log_lik_gra.R
@@ -1,11 +1,19 @@
 gpr_log_lik_gra <- function(m, s2, t2, phi, y, D){
   # Auxiliary Variables
-  Sig     <- t2 * exp(- phi * D) + s2 * diag(N)
+  C       <- exp(- phi * D)
+  K       <- t2 * C
+  Sig     <- K + s2 * diag(N)
   Sig_Inv <- solve(Sig)
   r       <- Sig_Inv %*%  (y - m * rep(1, N))
   N       <- length(y)
   # Derivative of s2
-  g_s2 <- - sum(diag(Sig_Inv)) / 2 
-  g_s2 <- g_s2 - sum(r^2) / 2
-  return(g_s2)
+  g_s2 <- - sum(diag(Sig_Inv)) / 2
+  g_s2 <-   g_s2 + sum(r^2) / 2
+  # Derivative of t2
+  g_t2 <- - sum(Sig_Inv * C) / 2
+  g_t2 <-   g_t2 + t(r) %*% C %*% r / 2
+  # Derivative of phi
+  g_phi <- sum(diag(Sig_Inv * K * D)) / 2
+  g_phi <- g_phi - t(r) %*% (K * D) %*% r / 2
+  return(c(g_s2, g_t2, g_phi))
 }
---
+++ gpr_log_lik_gra_tra.R
@@ -0,0 +1,22 @@
+gpr_log_lik_gra_tra <- function(m, a, b, c, y, D){
+  # Auxiliary Variables
+  s2      <- exp(a)
+  t2      <- exp(b)
+  phi     <- exp(c)
+  C       <- exp(- phi * D)
+  K       <- t2 * C
+  Sig     <- K + s2 * diag(N)
+  Sig_Inv <- solve(Sig)
+  r       <- Sig_Inv %*%  (y - m * rep(1, N))
+  N       <- length(y)
+  # Derivative of s2
+  g_a <- - sum(diag(Sig_Inv) * s2) / 2
+  g_a <-   g_a + sum(r^2) * s2 / 2
+  # Derivative of t2
+  g_b <- - sum(Sig_Inv * K) / 2
+  g_b <-   g_b + t(r) %*% K %*% r / 2
+  # Derivative of phi
+  g_c <- sum(diag(Sig_Inv * K * D)) * phi / 2
+  g_c <- g_c - t(r) %*% (K * D) %*% r * phi / 2
+  return(g_c)
+}
