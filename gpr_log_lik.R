94bddbc8432c06dc60311cad9d0f89c6850030b6
unknown <rgutie17@ucsc.edu>
2023-06-13 21:09:17 -0500

    Multi-location GPR Log-likelihood

--- gpr_log_lik.R
+++ gpr_log_lik.R
@@ -1,6 +1,6 @@
 gpr_log_lik <- function(m, s2, t2, phi, y, D){
-  N   <- length(y)
+  N   <- dim(y)[2]
   Sig <- t2 * exp(- phi * D) + s2 * diag(N)
-  ll  <- mvtnorm::dmvnorm(x = y, m = rep(1, N) * m, sigma = Sig, log = TRUE)
+  ll  <- sum(mvtnorm::dmvnorm(x = y, m = rep(1, N) * m, sigma = Sig, log = TRUE))
   return(ll)
 }
